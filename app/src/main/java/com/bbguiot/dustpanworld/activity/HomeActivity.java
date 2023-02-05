67e70bfa2face024904c4b5ab1ff36ea6d9183db
WangEvolve <w35417091@gmail.com>
2023-02-06 02:00:39 +0800

    2023-2-6 02:00:17 顶部和底部导航栏

--- app/src/main/java/com/bbguiot/dustpanworld/activity/HomeActivity.java
+++ app/src/main/java/com/bbguiot/dustpanworld/activity/HomeActivity.java
@@ -1,15 +1,54 @@
 package com.bbguiot.dustpanworld.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import com.bbguiot.dustpanworld.R;
+import com.bbguiot.dustpanworld.fragment.HomeFragment;
+import com.bbguiot.dustpanworld.fragment.ShopFragment;
+import com.bbguiot.dustpanworld.fragment.UserFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private Fragment selectFragment = new Fragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+//        进门默认选择首页
+        selectFragment = new HomeFragment();
+//        绑定导航栏组件
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectFragment).commit();
+
     }
+//       选择导航栏按钮的方法
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_home:
+                            selectFragment = new HomeFragment();
+                            break;
+                        case R.id.navigation_shop:
+                            selectFragment = new ShopFragment();
+                            break;
+                        case R.id.navigation_user:
+                            selectFragment = new UserFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectFragment).commit();
+                    return true;
+                }
+            };
 }
---
+++ app/src/main/java/com/bbguiot/dustpanworld/fragment/HomeFragment.java
@@ -0,0 +1,24 @@
+package com.bbguiot.dustpanworld.fragment;
+
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bbguiot.dustpanworld.R;
+
+/**
+ * @Date: 2023/2/6 0:15
+ * @Name: Wang Evolve
+ * @Email: w35417091@gmail.com
+ **/
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
