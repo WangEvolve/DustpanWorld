f69df3f988c0074334fb85e845458f705eefd635
Went Sisiter <2534895738@qq.com>
2023-02-16 17:33:36 +0800

    添加“登录”界面

---
+++ app/src/main/java/com/bbguiot/dustpanworld/activity/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.bbguiot.dustpanworld.activity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.AbsoluteSizeSpan;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bbguiot.dustpanworld.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText ed_phone;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //设置提示输入手机号码字体大小
+        findViewById(R.id.ed_phone);
+        SpannableString s = new SpannableString("请输入手机号码");
+        AbsoluteSizeSpan ts = new AbsoluteSizeSpan(14, true);
+        s.setSpan(ts, 0, s.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ed_phone.setHint(s);
+
+        //设置提示输入密码字体大小
+        ed_phone = findViewById(R.id.ed_password);
+        SpannableString s2 = new SpannableString("请输入密码");
+        AbsoluteSizeSpan ts2 = new AbsoluteSizeSpan(14, true);
+        s2.setSpan(ts2, 0, s2.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ed_phone.setHint(s2);
+    }
+
+
+    //输入手机号码验证
+
+}
