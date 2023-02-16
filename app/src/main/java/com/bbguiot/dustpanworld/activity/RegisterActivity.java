e339db107c71bd9b13bef4e9072a4c3294c48d2f
lstrawberry <2737473350@qq.com>
2023-02-16 23:04:12 +0800

    注册页面*（但点击登陆或者注册页面貌似没弄相互跳转）

--- app/src/main/java/com/bbguiot/dustpanworld/activity/RegisterActivity.java
+++ app/src/main/java/com/bbguiot/dustpanworld/activity/RegisterActivity.java
@@ -1,9 +1,56 @@
 package com.bbguiot.dustpanworld.activity;
 
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bbguiot.dustpanworld.R;
+
 /**
  * @Date: 2023/2/16 17:36
  * @Name: Went Sister
  * @Email: 2534895738@gmail.com
  **/
-public class RegisterActivity {
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    //验证手机号码
+    private void checkUSphoneNumber(String phoneNumber) {
+        if (TextUtils.isEmpty(phoneNumber)) {
+            Toast.makeText(RegisterActivity.this, "手机号码不能为空!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (phoneNumber.length() < 11) {
+            Toast.makeText(RegisterActivity.this, "手机号码长度为11个字符", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (phoneNumber.contains("@")) {
+            Toast.makeText(RegisterActivity.this, "手机号码不能包含特殊字符", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    //验证密码
+    private void checkUSPassword(String password, String rePassword) {
+        if (password.length() < 8||password.length()>20) {
+            Toast.makeText(RegisterActivity.this, "密码长度为8——20个字符", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(RegisterActivity.this, "密码不能为空!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!password.equals(rePassword)) {
+            Toast.makeText(RegisterActivity.this, "两次输入的密码不一致!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
 }
